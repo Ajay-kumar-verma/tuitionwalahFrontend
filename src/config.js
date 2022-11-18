f4c6870fc2b04bc84d66a44876820ad8e75181f7
Siddhantsachin <91889081+Siddhantsachin@users.noreply.github.com>
2022-11-19 04:46:44 +0530

    document

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const data  ={
-    ENDPOINT : 'https://twserver.herokuapp.com',
-    // ENDPOINT : 'localhost:4000'  
+    // ENDPOINT : 'https://twserver.herokuapp.com',
+    ENDPOINT : 'http://localhost:3001'  
     // ENDPOINT : 'https://twtest.herokuapp.com',
 
 
--- src/container/user/document/Document.js
+++ src/container/user/document/Document.js
@@ -32,7 +32,7 @@ const App = () => {
       <Upload
         action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
         listType="picture-card"
-        fileList={fileList}
+        // fileList={fileList}
         onChange={onChange}
         onPreview={onPreview}
       >

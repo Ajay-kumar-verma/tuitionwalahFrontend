7fd53300de5d34dd4cedaf06f6b8233c37d2f62c
Siddhantsachin <91889081+Siddhantsachin@users.noreply.github.com>
2022-12-01 03:16:15 +0530

    d

--- src/container/admin/Admin.js
+++ src/container/admin/Admin.js
@@ -1,36 +1,22 @@
-import React from 'react'
-import {NavLink ,useNavigate } from 'react-router-dom'
-import {Tooltip } from 'antd'; 
-import {Select} from 'antd';
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import {NavLink ,Outlet} from 'react-router-dom'
 import NavBar from '../nav/Nav'
 
-import { useSelector, useDispatch } from 'react-redux'
-import action from '../../rtk/actions/index'
-
+const list = (data)=>data.map(e=><NavLink  className="anchor" to={`${e}`} > {e} </NavLink>) 
+const data = [
+...list(["user","student","teacher","parent","agent",'team']),
+]
 
 
 const Admin = () => {
-  const dispatch = useDispatch() 
-  const state = useSelector(state=>state)
  
-
-
   return (
   <>
- {/* <Navbar data={['ajay','aja']} /> */}
- <nav>
-<NavLink to="user" >user </NavLink>
-<NavLink to="student" >student </NavLink>
-<NavLink to="teacher" >teacher </NavLink>
-<NavLink to="parent" >parent </NavLink>
-<NavLink to="setting" >setting </NavLink>
-<NavLink to="team" >team </NavLink>
-<NavLink to="logout" >logout</NavLink>
+   <nav>
+   <NavBar data={data} />
   </nav>
-<Outlet />
-</>    
-
+    <Outlet />
+    </>
     )
 }
 
--- src/container/user/User.js
+++ src/container/user/User.js
@@ -1,25 +1,23 @@
-import React,{useMemo} from 'react'
+import React from 'react'
 import {NavLink ,Outlet} from 'react-router-dom'
 import NavBar from '../nav/Nav'
 
 const list = (data)=>data.map(e=><NavLink  className="anchor" to={`${e}`} > {e} </NavLink>) 
+const data = [
+...list(["","home","contact","address","document",]),
+]
    
 const User = () => {
    
 
-     const data = [
-     ...list(["","home","contact","address","document",]),
-   ]
-
-   const navLists= useMemo(()=>{
-    return <NavBar data={data} />
-  },[])
-  
+   
+    
 
   return (
     <>
    <nav>
-   {navLists}
+   <NavBar data={data} />
+  
   </nav>
 <Outlet />
     </>

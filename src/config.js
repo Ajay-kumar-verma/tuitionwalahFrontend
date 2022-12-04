e34184c264583dde2c2547619147bb32e26c8b24
Siddhantsachin <91889081+Siddhantsachin@users.noreply.github.com>
2022-12-04 18:36:46 +0530

    ss

--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
 const data  ={
-    // ENDPOINT : 'https://twserver.herokuapp.com',
-    ENDPOINT : 'http://localhost:3001'  ,
-    // ENDPOINT : 'https://tws.onrender.com',
-    // ENDPOINT : 'https://twtest.herokuapp.com',
+    // ENDPOINT : 'http://localhost:3001'  ,
+    ENDPOINT : 'https://tws.onrender.com',
  clientId :
     '614668011518-d4g0cr825vs5ugm0iqgo5ieovqhgiisr.apps.googleusercontent.com'
    

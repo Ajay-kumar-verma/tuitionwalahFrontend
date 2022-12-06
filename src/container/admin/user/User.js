b89d5a6187e7df6d6b5e5365ca4fd9d3550730ba
Siddhantsachin <91889081+Siddhantsachin@users.noreply.github.com>
2022-12-07 01:46:51 +0530

    dd

--- src/container/admin/user/User.js
+++ src/container/admin/user/User.js
@@ -28,38 +28,62 @@ useEffect(() => {
     setData(users)
 
 },[state])
+ 
+const sort =({type}) =>{
+  const newData = [...data];
+  if(type==='reverse'){
+    setData(newData.reverse());
+    return ;
+  }
+  if(type==='name'){
+  newData.sort(function(a, b){
+    return a.FirstName.localeCompare(b.FirstName)
+  })
+  setData(newData); 
+}
+}  
+
+
 
   return (
-   <div 
-   className="form"
-   >
-        {contextHolder}
-      
-     {JSON.stringify(data[0])}
+   <div className="form">
+    {contextHolder}
+    <Button 
+     onClick={()=>{sort({type:'reverse'})}}>
+     reverse
+   </Button>
+
+   <Button 
+     onClick={()=>{sort({type:'name'})}}>
+  Name
+   </Button>
+
 
   <Collapse accordion>
     {data.map((e,i)=>{
-    const {FirstName,LastName,Mobile,Email} = e;  
-
+    const {FirstName,LastName,Mobile} = e;  
+   
+     const keys =Object.keys(e);
+    //  const values =Object.values(e);
+     
    return <Panel header={`${FirstName} ${LastName}`} 
      extra={Mobile}
        key={i}>
-    {/* <List 
+     <List 
       size="small"
       bordered
-      dataSource={[e]}
-      renderItem={(item) =>{
-        const key = Object.keys(item)[0];
-        const value = Object.values(item)[0];
+      dataSource={keys}
+      renderItem={(key) =>{
+        let value =e[key] 
+         if('TimeAtCreated'===key || 'DateOfBirth'===key)
+            value = moment(value).format("dddd, MMMM Do YYYY, h:mm:ss a")
         return <Row justify="space-between">
-        <Col style={{backgroundColor:"#fff111"}} span={8}><List.Item>{key}</List.Item></Col>
+        <Col span={8}><List.Item>{key}</List.Item></Col>
         <Col span={16}><List.Item>{value}</List.Item></Col>
     </Row>        }
       }   
    />    
-     */}
-   {JSON.stringify(e)}
-   </Panel>        
+    </Panel>        
 }
  )}
   

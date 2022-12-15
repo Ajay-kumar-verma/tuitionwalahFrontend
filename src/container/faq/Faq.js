7913f569e220203aed1d9d449f0d1e3f6c33ab1a
Siddhantsachin <91889081+Siddhantsachin@users.noreply.github.com>
2022-12-15 16:05:12 +0530

    dfd

--- src/container/faq/Faq.js
+++ src/container/faq/Faq.js
@@ -3,15 +3,25 @@ import { Collapse } from 'antd';
 const { Panel } = Collapse;
 
 const App = () => (
-  <Collapse className="form"  accordion>
+  <Collapse className="form"  style={{marginTop:"50px"}} accordion>
     <Panel header="How we work !" key="1">
      We developed platform where Students ,teachers and Parents can connect . 
     </Panel>
     <Panel header="Who we are !" key="2">
      We are engineers !
     </Panel>
+
     <Panel header="What our aim is !" key="3">
-     Providing Tutor for all required student  at their Home .  </Panel>
+     Providing Tutor for all required student  at their Home . 
+      </Panel>
+    <Panel header="How many students has  joinned so far !  " key="4">
+    More than 500   
+     </Panel>
+  
+      <Panel header="How many teacher has  joinned so far ! " key="5">
+     more than 150  
+      </Panel>
+  
   </Collapse>
 );
 export default App;

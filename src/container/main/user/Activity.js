e085e0d8f0323579f653b9b52716e5c387f671b0
Siddhantsachin <91889081+Siddhantsachin@users.noreply.github.com>
2022-12-20 21:05:58 +0530

    ff

--- src/container/main/user/Activity.js
+++ src/container/main/user/Activity.js
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
-import { Button, Modal,Select   } from 'antd';
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import action from '../../../rtk/actions'
+import { Button, Modal,Select ,message
+  } from 'antd';
 const options = [];
 
 ['admin','main','lead','user','agent'].forEach(e=>
@@ -14,16 +17,34 @@ const handleChange = (value) => {
 
 const App = ({user}) => {
   const [open, setOpen] = useState(false);
+   const [userType, setUserType] = useState([])
+  const [messageApi, contextHolder] = message.useMessage()
+  const dispatch = useDispatch()
+  const state =useSelector(({ main: { permission } })=> permission)
+  const {main:{permission}} = action
+
+  console.log({ state ,permission})
+
+  useEffect(() => {
+    const { data, loading } = state
+    let { message, recieved } = data
+    if (!loading && recieved)
+     messageApi.open({ type: 'success', content: message })
+    if (loading) messageApi.open({ type: 'loading', content: `please wait` })
+
+  },[state])
+
   return (
     <>
+    {contextHolder}
       <Button style={{width:"50%"}} block onClick={() => setOpen(true)}>
       Activity
       </Button>
       <Modal
-        title="Modal 1000px width"
+        title="Permission and previllage "
         centered
         open={open}
-        onOk={() => setOpen(false)}
+        onOk={() =>dispatch(permission({userType,user})) }
         onCancel={() => setOpen(false)}
         width={1000}
       >
@@ -35,7 +56,7 @@ const App = ({user}) => {
       width: '100%',
     }}
     placeholder="Tags Mode"
-    onChange={handleChange}
+    onChange={(e)=>setUserType(e)}
     defaultValue={user.userType}
     options={options}
   />
--- src/rtk/features/main/Main.js
+++ src/rtk/features/main/Main.js
@@ -5,13 +5,14 @@ const {createAsyncThunk,createSlice} = require('@reduxjs/toolkit');
 const initialState = {
    all:{},
     user:{loading:false,apiCall:false, data:{}},
+    permission:{loading:false,apiCall:false, data:{}},
   }
   
     const user = createAsyncThunk(
       'user',
       async (obj)=>{
       try {
-       const {data} = await  api().get(`/main/user`,obj)
+       const {data} = await  api().get(`/main/users`,obj)
            return data; 
       } catch (error) {
         console.log("error",error);
@@ -20,12 +21,27 @@ const initialState = {
           }
  )
 
+  
+ const permission = createAsyncThunk(
+  'permission',
+  async (obj)=>{
+    // console.log({obj})
+    try {
+   const {data} = await  api().post(`/main/permission`,obj)
+       return data; 
+  } catch (error) {
+    console.log("error",error);
+    return error;   
+  }
+      }
+)
+
 
 const {reducer } = createSlice({
   name:"main",
   initialState,
   extraReducers:{
-    [user.pending]: state => {
+   [user.pending]: state => {
       state.user.loading = true;
    },
    [user.fulfilled]:(state,{payload}) => {
@@ -44,10 +60,27 @@ const {reducer } = createSlice({
      state.message ="request rejected ! ";
     },  
   
+   [permission.pending]: state => {
+      state.permission.loading = true;
+   },
+   [permission.fulfilled]:(state,{payload}) => {
+     state.all = payload;
+     state.permission.loading = false ;
+     state.permission.data=payload;
+     state.permission.apiCall=false;
+    },
+   [user.rejected]:(state,{payload}) => {
+     state.permission =payload; 
+     state.permission.loading = false;
+     state.error = payload?.error;
+     state.permission.data=payload;
+     state.message ="request rejected ! ";
+    },  
+    
   } ,  
 
 })
 
 export const mainReducer =  reducer ;
 
-export const mainActions = { user };
+export const mainActions = { user ,permission};

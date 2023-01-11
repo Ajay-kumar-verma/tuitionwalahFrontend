033a09b7d70596df61ccfe1b4cf9ae83f18b7301
Ajay <ajay997354@gmail.com>
2023-01-10 23:20:52 -0800

    ww

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const data = {
   //  ENDPOINT : 'http://localhost:3001'  ,
-  ENDPOINT: 'https://twss.onrender.com',
+  ENDPOINT: 'https://tws.onrender.com',
   clientId:
     '614668011518-d4g0cr825vs5ugm0iqgo5ieovqhgiisr.apps.googleusercontent.com',
 }
